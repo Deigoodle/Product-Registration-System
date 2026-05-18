433a3ed55133ce8e6639e60d4ae06da3f66a4db5
Deigoodle <diego.nicocastillo@gmail.com>
2026-05-18 18:37:38 -0400

    docker compose (only postgres)

--- sql/sample.sql
+++ sql/sample.sql
@@ -65,3 +65,33 @@ CREATE TRIGGER trigger_update_fecha_actualizacion
 BEFORE UPDATE ON productos
 FOR EACH ROW
 EXECUTE FUNCTION update_fecha_actualizacion();
+
+-- Populate DB --
+
+INSERT INTO monedas (nombre, codigo) VALUES
+('Dólar Estadounidense', 'USD'),
+('Euro', 'EUR'),
+('Peso Chileno', 'CLP');
+
+INSERT INTO bodegas (nombre) VALUES
+('Bodega Central'),
+('Bodega Norte'),
+('Bodega Sur');
+
+INSERT INTO sucursales (nombre, bodega_id) VALUES
+('Sucursal C1', 1),
+('Sucursal C2', 1),
+('Sucursal C3', 1),
+('Sucursal N1', 2),
+('Sucursal N2', 2),
+('Sucursal N3', 2),
+('Sucursal S1', 3),
+('Sucursal S2', 3),
+('Sucursal S3', 3);
+
+INSERT INTO materiales (nombre) VALUES
+('Plástico'),
+('Metal'),
+('Madera'),
+('Vidrio'),
+('Textil');
